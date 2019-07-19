ebd6edf7354efbe280e97b3cd7bcf4343df2e987
Duro Akintunde <duroak@yahoo.com>
2019-07-19 08:59:44 -0500

    updates

--- index.js
+++ index.js
@@ -8,4 +8,38 @@ const navSlide = () => {
 
 } 
 
-navSlide();
+function switchHome () {
+    $('#js-home').click(function(event){
+        $('#bioView').hide;
+        $('#projectView').hide;
+        $('#homeView').show;
+        $('.footer').hide;
+    });
+}
+
+function switchBio () {
+    $('.navbar').on('click', '#js-bio', function(event){
+        $('#bioView').show;
+        $('#projectView').hide;
+        $('#homeView').hide;
+        $('.footer').show;
+    });
+}
+function switchProjects () {
+    $('#js-projects').click(function(event){
+        $('#bioView').hide;
+        $('#projectView').show;
+        $('#homeView').hide;
+        $('.footer').show;
+    });
+}
+
+function initializePage(){
+    navSlide();
+    switchHome();
+    switchBio();
+    switchProjects();
+}
+
+$(initializePage);
+
